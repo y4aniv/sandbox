24c0fe9976d81ebbb1cf3f513e0dc067fe0a8b8d
Yaniv <120222659+y4aniv@users.noreply.github.com>
2023-11-23 11:36:13 +0000

    oops!

--- yoda-python/main.py
+++ yoda-python/main.py
@@ -1,39 +1,22 @@
-def Split(string, sep):
-    
-    """Fonction qui sépare une chaîne de caractères en fonction d'un séparateur sans utiliser
-    Entrée: string (str), sep (str)
-    Sortie: liste (list)"""
-
-    liste = []
-    mot = ""
-    for i in string:
-        if i == sep:
-            liste.append(mot)
-            mot = ""
-        else:
-            mot += i
-    liste.append(mot)
-    return liste
-
 def SVC_to_CSV(SVC):
-
+    
     """Transforme une phrase de la forme Sujet Verbe Complément en Complément Sujet Verbe
-    Exemple: Je suis beau -> Beau je suis
+    Exemple: "Je suis beau" -> "Beau Je suis"
     Entrée: SVC (str)
-    Sortie: CSV (str)"""
+    Sortie: CSV (str)
+    """
 
-    SVC = Split(SVC, " ")
-    S = SVC[0]
-    V = SVC[1]
-    C = SVC[2].capitalize()
-    CSV = f'{C} {S} {V}'
+    s1 = SVC.index(" ")
+    s2 = SVC[s1+1:].index(" ")+s1+1
 
-    return CSV
+    Sujet = SVC[0:s1]
+    Verbe = SVC[s1+1:s2]
+    Complement = SVC[s2+1:].capitalize()
 
+    return f'{Complement} {Sujet} {Verbe}'
 
 # SVC = input("Phrase (Sujet Verbe Complément) >>> ")
 # print(f"Phrase (Complément Sujet Verbe) >>> {SVC_to_CSV(SVC)}")
-
 
 assert SVC_to_CSV("Je suis beau") == "Beau Je suis"
 assert SVC_to_CSV("Tu manges demain") == "Demain Tu manges"
